d07778d7daa4c788f862469dcd38239e773b67be
Yeap Lee Zhi <bitonana@gmail.com>
2023-04-16 15:26:12 +0800

    Fix sidebar scrolling issue

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
-function openNav() {
+function openNav(event) {
+    event.preventDefault();
     document.querySelector(".sidenav").style.width = "100%";
 }
 
-function closeNav() {
+function closeNav(event) {
+    event.preventDefault();
     document.querySelector(".sidenav").style.width = "0";
 }
+
+const menuNav = document.querySelector(".menu-nav");
+menuNav.addEventListener('click', e => openNav(e));
+
+const closeBtn = document.querySelector(".closebtn");
+closeBtn.addEventListener('click', e => closeNav(e));
