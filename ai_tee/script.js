08526c227b526050e91db4457cf2db550a0924fa
Yeap Lee Zhi <bitonana@gmail.com>
2023-04-22 17:25:05 +0800

    Added fade in animation for AI-Tee

--- ai_tee/script.js
+++ ai_tee/script.js
@@ -5,3 +5,24 @@
 // navLinks.forEach((l) => {
 //     l.addEventListener('click', () => { bsCollapse.toggle() })
 // })
+
+function animateOnScroll() {
+    const fadeInElements = document.querySelectorAll("p, h1, h5");
+
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach((entry) => {
+            // Get the index of the element in the parent node and delay the animation so that it appears one after the other
+            const children = Array.from(entry.target.parentNode.children);
+            let delay = children.indexOf(entry.target) * 0.2;
+
+            if (entry.isIntersecting) {
+                entry.target.classList.add('animate__animated', 'animate__fadeIn');
+                entry.target.style.animationDelay = `${delay}s`;
+            }
+        });
+    });
+
+    fadeInElements.forEach((el) => observer.observe(el));
+}
+
+animateOnScroll();
